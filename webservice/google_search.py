c87f3fc3e431969d7291725d6ae3b34e3a0a7e46
Chih Chiu <chih.chiu.19@gmail.com>
2020-05-10 19:52:45 -0700

    add google search module

--- webservice/google_search.py
+++ webservice/google_search.py
@@ -1,8 +1,38 @@
-"""Provides basic Google search functionalities."""
+"""Provides basic Google search functionality.
 
-from googlesearch import search, get_tbs
+The `googlesearch` module is installed via `pip install google`; reference:
+https://python-googlesearch.readthedocs.io/en/latest/index.html#module-googlesearch
+"""
+import datetime
+from typing import Iterable, Text
+
+import googlesearch
 
 
-def QuickSearchUrls() -> Iterable[Text]:
-  results = search('gold price', lang="en", tbs=tbs, country="usa", tpe="nws",
-                   stop=100)
+def QuickSearchUrls(
+    query: Text,
+    from_date: datetime.date,
+    to_date: datetime.date = None,
+    lang: Text = 'en',
+    country: Text = 'usa',
+    number_of_results: int = 100,
+) -> Iterable[Text]:
+  """Performs a Google news search using a query.
+
+  Args:
+    query: search query.
+    from_date: search news from this date.
+    to_date: search news until this date, default to today.
+    lang: search news in this language.
+    country: search news in this country.
+    number_of_results: number of results to return.
+
+  Returns:
+    A list of searched result URLs.
+  """
+  if not to_date:
+    to_date = datetime.date.today()
+  tbs = googlesearch.get_tbs(from_date, to_date)
+  return googlesearch.search(
+    query, lang=lang, tbs=tbs, country=country, tpe="nws",
+    stop=number_of_results)
