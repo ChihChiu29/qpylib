a3bedbaa119a07f79983e8468ac090441b206763
Chih Chiu <chih.chiu.19@gmail.com>
2021-03-21 16:08:24 -0700

    made twitter to work with twint

--- webservice/twitter.py
+++ webservice/twitter.py
@@ -1,52 +1,83 @@
 """Provides helpers interacting with Twitter."""
 import datetime
-from typing import List, Text
+import os
+import pandas
+from typing import Dict, Iterable, List, Text
 
-import GetOldTweets3
+import nest_asyncio
+import twint
 
-DATE_FORMAT = '%Y-%m-%d'
+from qpylib import storage
 
 
 def Search(
     query: Text,
     from_date: datetime.datetime = None,
     to_date: datetime.datetime = None,
-    number_of_results: int = 10,
-) -> List[GetOldTweets3.models.Tweet]:
+    number_of_results: int = 100,
+) -> pandas.DataFrame:
   """Search tweets.
-
-  Reference:
-  * https://libraries.io/pypi/GetOldTweets3
-  * https://github.com/Jefferson-Henrique/GetOldTweets-python
 
   Args:
     query: the search query.
-    from_date: a date string like "2019-01-01". Leaving as None to not set it.
-    to_date: a date string like "2019-01-01". Leaving as None to not set it.
+    from_date: search from this datetime.
+    to_date: search till this datetime.
     number_of_results: number of results to return.
 
   Returns:
-    A list of tweets. Each tweet has attributes:
-      'author_id',
-      'date',
-      'favorites',
-      'formatted_date',
-      'geo',
-      'hashtags',
-      'id',
-      'mentions',
-      'permalink',
-      'replies',
-      'retweets',
-      'text',
-      'to',
-      'urls',
-      'username'
+    A dataframe of tweets. For columns, reference:
+      {
+        'id': 1371248526085226496,
+        'conversation_id': '1371248036563795969',
+        'created_at': '2021-03-14 23:54:59 UTC',
+        'date': '2021-03-14',
+        'time': '23:54:59',
+        'timezone': '+0000',
+        'user_id': 1233956153656332291,
+        'username': 'je4ia',
+        'name': 'funy guy sbungbob',
+        'place': '',
+        'tweet': '@Zer0Priv And stock up on Bitcoin and GameStop stocks',
+        'language': 'en',
+        'mentions': [],
+        'urls': [],
+        'photos': [],
+        'replies_count': 0,
+        'retweets_count': 0,
+        'likes_count': 2,
+        'hashtags': [],
+        'cashtags': [],
+        'link': 'https://twitter.com/je4ia/status/1371248526085226496',
+        'retweet': False,
+        'quote_url': '',
+        'video': 0,
+        'thumbnail': '',
+        'near': '',
+        'geo': '',
+        'source': '',
+        'user_rt_id': '',
+        'user_rt': '',
+        'retweet_id': '',
+        'reply_to': [{'screen_name': 'Zer0Priv',
+          'name': 'Zer0',
+          'id': '1256485417744031747'}],
+        'retweet_date': '',
+        'translate': '',
+        'trans_src': '',
+        'trans_dest': '',
+      },
   """
-  tweetCriteria = GetOldTweets3.manager.TweetCriteria().setQuerySearch(query)
+  nest_asyncio.apply()
+
+  c = twint.Config()
+  c.Search = query
   if from_date:
-    tweetCriteria.setSince(from_date.strftime(DATE_FORMAT))
+    c.Since = from_date.strftime('%Y-%m-%d %H:%M:%S')
   if to_date:
-    tweetCriteria.setUntil(to_date.strftime(DATE_FORMAT))
-  tweetCriteria.setMaxTweets(number_of_results)
-  return GetOldTweets3.manager.TweetManager.getTweets(tweetCriteria)
+    c.Until = to_date.strftime('%Y-%m-%d %H:%M:%S')
+  c.Limit = number_of_results
+  c.Pandas = True
+  c.Hide_output = True
+  twint.run.Search(c)
+  
+  return twint.storage.panda.Tweets_df
